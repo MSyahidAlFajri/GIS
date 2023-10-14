78b06d30bdbadc9654f9d86500b07b272550aeaa
MSyahidAlFajri <masyahida963@gmail.com>
2023-10-14 12:47:04 +0700

    chpp3

---
+++ chapter3/js/template/template.js
@@ -0,0 +1,15 @@
+export let URLGeoJson = "https://asia-southeast2-rock-prism-401900.cloudfunctions.net/chapter3gis";
+export let tableTag="tr";
+export let tableRowClass="content is-small";
+export let tableTemplate=`
+<td>#NAME#</td>
+<td >#KORDINAT#</td>
+<td>#TYPE#</td>
+`
+export const clickpopup = `
+Long : #LONG#<br>
+Lat  : #LAT#<br>
+X   : #X#<br>
+Y   : #Y#<br>
+HDMS : #HDMS#<br>
+`
