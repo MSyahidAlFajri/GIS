90406a17e373405f15bcfe0c4342fe9306fc6c7f
MSyahidAlFajri <masyahida963@gmail.com>
2023-10-14 12:24:23 +0700

    ahhhh

--- chapter3/croot.js
+++ chapter3/croot.js
@@ -13,17 +13,17 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     const waypointSource = new ol.source.Vector({
-        url: 'Aceh.json',
+        url: 'https://asia-southeast2-rock-prism-401900.cloudfunctions.net/chapter3gis',
         format: new ol.format.GeoJSON()
     });
 
     const lineStringSource = new ol.source.Vector({
-        url: 'Aceh.json',
+        url: 'https://asia-southeast2-rock-prism-401900.cloudfunctions.net/chapter3gis',
         format: new ol.format.GeoJSON()
     });
 
     const polylineSource = new ol.source.Vector({
-        url: 'Aceh.json',
+        url: 'https://asia-southeast2-rock-prism-401900.cloudfunctions.net/chapter3gis',
         format: new ol.format.GeoJSON()
     });
 
@@ -111,7 +111,7 @@ document.addEventListener('DOMContentLoaded', () => {
 document.addEventListener("DOMContentLoaded", () => {
     const pointTable = document.getElementById("pointTable").getElementsByTagName('tbody')[0];
 
-    fetch("Aceh.json") // Ganti "data.json" dengan nama file JSON Anda
+    fetch("https://asia-southeast2-rock-prism-401900.cloudfunctions.net/chapter3gis") // Ganti "data.json" dengan nama file JSON Anda
         .then(response => response.json())
         .then(data => {
             data.features.forEach(feature => {
@@ -133,7 +133,7 @@ document.addEventListener("DOMContentLoaded", () => {
 document.addEventListener("DOMContentLoaded", () => {
     const pointTable = document.getElementById("polygonTable").getElementsByTagName('tbody')[0];
 
-    fetch("Aceh.json") // Ganti "data.json" dengan nama file JSON Anda
+    fetch("") // Ganti "data.json" dengan nama file JSON Anda
         .then(response => response.json())
         .then(data => {
             data.features.forEach(feature => {
@@ -155,7 +155,7 @@ document.addEventListener("DOMContentLoaded", () => {
 document.addEventListener("DOMContentLoaded", () => {
     const pointTable = document.getElementById("polylineTable").getElementsByTagName('tbody')[0];
 
-    fetch("Aceh.json") // Ganti "data.json" dengan nama file JSON Anda
+    fetch("") // Ganti "data.json" dengan nama file JSON Anda
         .then(response => response.json())
         .then(data => {
             data.features.forEach(feature => {
