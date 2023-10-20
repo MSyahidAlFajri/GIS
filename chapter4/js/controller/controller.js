ef2c6e92f1d954e390c35d49f33aa0fa01a897ca
MSyahidAlFajri <masyahida963@gmail.com>
2023-10-21 03:29:29 +0700

    baruuuu

--- chapter4/js/controller/controller.js
+++ chapter4/js/controller/controller.js
@@ -1,6 +1,8 @@
 import {setInner,addChild } from "https://jscroot.github.io/element/croot.js";
 import {tableTemplate, tableRowClass, tableTag} from "../template/template.js";
 import {map} from '../config/configpeta.js';
+import Draw from 'https://cdn.skypack.dev/ol/interaction/Draw.js';
+
 
 export function isiRowPoint(value){
     if (value.geometry.type === "Point") {
@@ -45,6 +47,57 @@ export function MakeGeojsonFromAPI(value) {
     // document.body.appendChild(link);
 
     return link;
+}
+export function drawer(geojson) {
+    const source = new ol.source.Vector({
+        wrapx: false
+      });
+      const Stroke = new ol.layer.Vector({
+        source: source,
+        style: function (feature) {
+            const featureType = feature.getGeometry().getType();
+            if (featureType === 'Polygon') {
+                return new ol.style.Style({
+                    stroke: new ol.style.Stroke({
+                        color: 'blue', 
+                        width: 2
+                    })
+                });
+            } else {
+                
+                return new ol.style.Style({
+                    stroke: new ol.style.Stroke({
+                        color: 'red', 
+                        width: 3
+                    })
+                });
+            }
+        }
+    });
+
+    const typeSelect = document.getElementById('type');
+
+    let draw; // global so we can remove it later
+    typeSelect.onchange = function () {
+    map.removeInteraction(draw);
+    addInteraction();
+    };
+
+    document.getElementById('undo').addEventListener('click', function () {
+    draw.removeLastPoint();
+    });
+    function addInteraction() {
+        const value = typeSelect.value;
+        if (value !== 'None') {
+            draw = new Draw({
+            source: source,
+            type: typeSelect.value,
+            });
+            map.addInteraction(draw);
+        }
+        }
+    addInteraction();
+    map.addLayer(Stroke);
 }
 
 
--- chapter4/js/main.js
+++ chapter4/js/main.js
@@ -23,57 +23,6 @@ get(URLGeoJson,data => {
     // console.log(link)
     // console.log(geojson)
     AddLayerToMAP(link)
-    // drawer(link)
+    drawer(link)
 }); 
-    // export function drawer(geojson) {
-    //     const source = new ol.source.Vector({
-    //         wrapx: false
-    //       });
-    //       const Stroke = new ol.layer.Vector({
-    //         source: source,
-    //         style: function (feature) {
-    //             const featureType = feature.getGeometry().getType();
-    //             if (featureType === 'Polygon') {
-    //                 return new ol.style.Style({
-    //                     stroke: new ol.style.Stroke({
-    //                         color: 'blue', 
-    //                         width: 2
-    //                     })
-    //                 });
-    //             } else {
-                    
-    //                 return new ol.style.Style({
-    //                     stroke: new ol.style.Stroke({
-    //                         color: 'red', 
-    //                         width: 3
-    //                     })
-    //                 });
-    //             }
-    //         }
-    //     });
-    
-    //     const typeSelect = document.getElementById('type');
-    
-    //     let draw; // global so we can remove it later
-    //     typeSelect.onchange = function () {
-    //     map.removeInteraction(draw);
-    //     addInteraction();
-    //     };
-    
-    //     document.getElementById('undo').addEventListener('click', function () {
-    //     draw.removeLastPoint();
-    //     });
-    //     function addInteraction() {
-    //         const value = typeSelect.value;
-    //         if (value !== 'None') {
-    //             draw = new Draw({
-    //             source: source,
-    //             type: typeSelect.value,
-    //             });
-    //             map.addInteraction(draw);
-    //         }
-    //         }
-    //     addInteraction();
-    //     map.addLayer(Stroke);
-    // }
-    
+   
